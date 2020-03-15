e4065476dccac42422975b25039ad251685ca2dd
Timothy Mwiti <mwititimothyk@gmail.com>
2020-03-15 17:32:18 -0400

    Build basic game logic

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -1,35 +1,146 @@
+//
+//  GameOfLife.cpp
+//  GameOfLife
+//
+//  Created by Timothy Mwiti on 2/3/20.
+//  Copyright © 2020 tim. All rights reserved.
+//
+
+#include <stdio.h>
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include <iostream>
+#define WIDTH 100
+#define HEIGHT 100
 
-class Example : public olc::PixelGameEngine {
-    public:
-        Example() {
-            sAppName = "Example";
+class GameOfLife : public olc::PixelGameEngine {
+public:
+    GameOfLife() {
+        sAppName = "Game of Life";
+    }
+private:
+    bool curGen[WIDTH][HEIGHT];
+    bool nextGen[WIDTH][HEIGHT];
+    bool useCurToUpdate;
+    bool patternDrawingComplete;
+
+
+
+public:
+    bool OnUserCreate() override {
+        useCurToUpdate = true;
+        patternDrawingComplete = false;
+        Clear(olc::WHITE);
+        
+        for(int i = 0; i < WIDTH; i++){
+            for(int j = 0; j < HEIGHT; j++){
+                curGen[i][j] = false;
+            }
+        }
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override {
+        if(GetKey(olc::Key::A).bPressed){
+            patternDrawingComplete = true;
+        }else if(GetKey(olc::Key::Q).bPressed){ // Exit Game
+            return false;
         }
 
-    public:
-        bool OnUserCreate() override {
-            // Called once at the start, so create things here
-            return true;
-        }
+        if(! patternDrawingComplete){
+            getUserPatternInput();
+        }else{
+            Clear(olc::WHITE);
+            std::chrono::milliseconds timespan(200);
+            std::this_thread::sleep_for(timespan);
+            for (int x = 0; x < ScreenWidth(); x++){
+                for (int y = 0; y < ScreenHeight(); y++){
+                    if(useCurToUpdate){
+                        nextGen[x][y] = getCellState(x, y, curGen[x][y], true);
+                    }else{
+                        curGen[x][y] = getCellState(x, y, nextGen[x][y], true);;
+                    }
+                }
+            }
 
-        bool OnUserUpdate(float fElapsedTime) override {
-            Clear(olc::BLACK);
-            // called once per frame
-            for (int x = 0; x < ScreenWidth(); x++)
-                for (int y = 0; y < ScreenHeight(); y++)
-                    Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));
+            if(useCurToUpdate){
+                drawPattern(nextGen);
+            }else{
+                drawPattern(curGen);
+            }
             
-            FillRect(GetMouseX(), GetMouseY(), 1, 1);
-            return true;
+            useCurToUpdate = !useCurToUpdate;
         }
+        return true;
+    }
+    
+    void getUserPatternInput(){
+        if(GetMouse(0).bHeld){
+            int mouseX = GetMouseX();
+            int mouseY = GetMouseY();
+            curGen[mouseX][mouseY] = true;
+        }
+
+
+        for(int x = 0; x < WIDTH; x++){
+            for(int y = 0; y < HEIGHT; y++){
+                if(curGen[x][y]){
+                    Draw(x, y, olc::BLACK);
+                }
+            }
+        }
+    }
+    
+    void drawPattern(bool gen[WIDTH][HEIGHT]){
+        for(int x = 0; x < WIDTH; x++){
+            for(int y = 0; y < HEIGHT; y++){
+                if(gen[x][y]){
+                    Draw(x, y, olc::BLACK);
+                }
+            }
+        }
+    }
+
+    bool getCellState(int w, int h, bool curState, bool useCur){
+        int aliveCount = 0;
+        if(useCur){
+            for(int i = w-1; i <= w+1; i++){
+                for(int j = h-1; j <= h+1; j++){
+                    if(i == w && j == h){
+                        continue;
+                    }
+                    if(i > -1 && i < WIDTH-1 && j > -1 && j < HEIGHT-1 && curGen[i][j]){
+                        aliveCount++;
+                    }
+                }
+            }
+        }else{
+            for(int i = w-1; i <= w+1; i++){
+                for(int j = h-1; j <= h+1; j++){
+                    if(i == w && j == h){
+                        continue;
+                    }
+                    if(i > -1 && i < WIDTH-1 && j > -1 && j < HEIGHT-1 && nextGen[i][j]){
+                        aliveCount++;
+                    }
+                }
+            }
+        }
+
+        if(curState){
+            return (aliveCount > 1 && aliveCount < 4) ? true : false;
+        }
+
+        return (aliveCount == 3) ? true : false;
+    }
+
 };
 
 
 int main(int argc, char const *argv[]) {
-	Example demo;
-	if (demo.Construct(15, 10, 32, 32))
-		demo.Start();
+    GameOfLife demo;
+    if (demo.Construct(WIDTH, HEIGHT, 10, 10))
+        demo.Start();
 
-	return 0;
+    return 0;
 }
